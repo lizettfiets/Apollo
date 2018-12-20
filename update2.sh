5caf0c010a2f9f508b91d1aed8978622b0b24d31
Rostyslav Golda <tx_hv@ukr.net>
2018-12-20 19:15:46 +0200

    1.24.4-macos

--- update2.sh
+++ update2.sh
@@ -11,22 +11,22 @@ then
       VERSION=$(cat VERSION)
 
     rm -rf $1/jre
-
+    rm -rf $1/lib
     
     if [[ "$unamestr" == 'Linux' ]]; then
-	curl -o jre.tar.gz https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz
-	tar -zxvf jre.tar.gz
-	mv jdk-11.0.1 jre
+	curl -o $1/jre.tar.gz https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz
+	tar -C $1 -zxvf $1/jre.tar.gz 
+	mv $1/jdk-11.0.1 $1/jre
     fi
     
     if [[ "$unamestr" == 'Darwin' ]]; then
-	curl -o https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_osx-x64_bin.tar.gz
-	tar -zxvf jre.tar.gz
-	mv jdk-11.0.1/Contents/Home jre
-	rm -rf jdk-11.0.1
+	curl -o $1/jre.tar.gz https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_osx-x64_bin.tar.gz
+	tar -C $1 -zxvf jre.tar.gz
+	mv $1/jdk-11.0.1/Contents/Home $1/jre
+	rm -rf $1/jdk-11.0.1
     fi
     
-    rm jre.tar.gz
+    rm $1/jre.tar.gz
 
     if [[ "$unamestr" == 'Darwin' ]]; then
 	chmod 755 $1/jre/bin/* $1/jre/lib/lib*
@@ -35,9 +35,9 @@ then
 	chmod 755 $1/jre/bin/*
     fi
     
-    curl -o libs.tzr.gz https://s3.amazonaws.com/updates.apollowallet.org/libs/ApolloWallet-$VERSION-libs.tar.gz
-    tar -zxvf ApolloWallet-$VERSION-libs.tar.gz
-    mv ApolloWallet-$VERSION-libs lib
+    curl -o $1/libs.tar.gz https://s3.amazonaws.com/updates.apollowallet.org/libs/ApolloWallet-$VERSION-libs.tar.gz
+    tar -C $1 -zxvf $1/libs.tar.gz
+    mv $1/ApolloWallet-$VERSION-libs $1/lib
 
 else
     echo Invalid input parameters $1
