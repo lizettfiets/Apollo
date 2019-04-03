19d5bbbe508a541a511bb4c240555d5ead8abeb5
Rostyslav Golda <tx_hv@ukr.net>
2019-04-03 04:45:05 +0300

    -sdfsag

--- src/main/java/com/apollocurrency/aplwallet/apl/Apl.java
+++ src/main/java/com/apollocurrency/aplwallet/apl/Apl.java
@@ -72,7 +72,7 @@ public final class Apl {
     private static Logger LOG;
 
 
-    public static final Version VERSION = Version.from("1.27.10");
+    public static final Version VERSION = Version.from("1.27.12");
 
     public static final String APPLICATION = "Apollo";
     private static Thread shutdownHook;
